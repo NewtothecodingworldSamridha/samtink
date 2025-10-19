b425034315af2368f6ba3c7e7bc12880decd323d
Samridha Banerjee <banerjeesamridha7@gmail.com>
2025-10-20 02:21:50 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🏆 SamTink Sports News</h1>
+        <p>Your Futuristic Sports News & Help Platform</p>
+        <p className="subtitle">Coming Soon...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
